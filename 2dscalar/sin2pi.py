da9419a86eaaa3652e0183067b201524aa41e776
Surya <you@example.com>
2025-08-04 10:47:05 +0530

    Updated domain to [-1,1]x[1,1]

--- 2dscalar/sin2pi.py
+++ 2dscalar/sin2pi.py
@@ -1,10 +1,13 @@
 import numpy as np
 from constants import *
 
-xmin, xmax = 0.0, 1.0
-ymin, ymax = 0.0, 1.0
+# xmin, xmax = 0.0, 1.0
+# ymin, ymax = 0.0, 1.0
+xmin, xmax = -1.0, 1.0
+ymin, ymax = -1.0, 1.0
 
 lvbc, rvbc, bhbc, thbc = periodic, periodic, periodic, periodic
 
 def initial_condition(x,y):
-    return np.sin(2*np.pi*x) * np.sin(2*np.pi*y)
+    # return np.sin(2*np.pi*x) * np.sin(2*np.pi*y)
+    return np.sin(np.pi*x) * np.sin(np.pi*y)
